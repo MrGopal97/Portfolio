96756b89ccccde3cb66b83b7d44b402d9b48e5ea
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-07-29 14:04:20 +0530

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Button from './Button'
+import ReactTypingEffect from 'react-typing-effect';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { NavLink } from 'react-router-dom';
+
+function Header({data}) {
+    return (
+        <>
+        <div className="header">
+            <img className="header__image" src="https://raw.githubusercontent.com/Ankit628792/ankitresume.tk/main/ankitresume.tk/assets/img/banner-image.png?token=AMROYHUOLPJZXPY7BLJF7FLBAOZ5I" alt="" />
+            <h1 className="big-h">
+                Hello! <br /> I'm Gopalr&nbsp;<br />
+                <span className="span-text">
+                    <ReactTypingEffect
+                        text={["Full Stack Developer", "Freelancer", "UI/UX Developer", "MERN Stack Developer"]}
+                    />
+                </span>
+                <br className="break" />Based in <span className="span-text">Tamil Nadu </span>
+            </h1>
+            <NavLink to="/contact">
+            <Button text="Let's Chat" />
+            </NavLink>
+            <ul className="social flex">
+                <li><a href="https://github.com/MrGopal97" target="_blank" rel="noreferrer"><GitHubIcon className="social__icon" /></a></li>
+                <li><a href="https://www.linkedin.com/in/mr-gopal-55250219a/" target="_blank" rel="noreferrer"><LinkedInIcon className="social__icon" /></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100011332176387" target="_blank" rel="noreferrer"><FacebookIcon className="social__icon" /></a></li>
+                <li><a href="https://www.instagram.com/k_r_i_s_h_30_07/" target="_blank" rel="noreferrer"><InstagramIcon className="social__icon" /></a></li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default Header
---
+++ src/data.js
@@ -0,0 +1,134 @@
+const data = {
+    "personal": {
+        "name": "Gopal",
+        "occupation": "I'm a Full Stack Developer, Freelancer, UI/UX Developer, MERN Stack Developer",
+        "email": "mr.gopal.in@gmail.com",
+        "number": "+91 6374607928",
+        "about": "Innovative, 6 month's experience, strong in design & development and integration with intuitive problem solving skills. Fully dedicated to work and have ability to solve different types of bugs and issues. Proficient in developing databases, creating interactive User Interfaces, writing and testing codes and implementing new features based on user feedback.",
+        "about2": " Seeking a beginner role to enhance and explore my knowledge in the business world. Understanding business goals and combining logic with aesthetics to create beautiful, consistent and functional design solutions",
+        // "website": "https://www.akresume.tk/",
+        "image": "Profile Pic.png",
+        "resumedownload": "https://drive.google.com/file/d/1L8EszxVaDoElwJALdc5YqaTmFNtwZ4zn/view",
+        "social": [
+            {
+                "name": "facebook",
+                "url": "",
+                "cName": "fas fa-facebook"
+            },
+            {
+                "name": "linkedin",
+                "url": "",
+                "cName": "fas fa-linkedin"
+            },
+            {
+                "name": "instagram",
+                "url": "",
+                "cName": "fas fa-instagram"
+            },
+            {
+                "name": "github",
+                "url": "",
+                "cName": "fas fa-github"
+            }
+        ]
+    },
+    "why": [
+        {
+            "title": "In-Depth Industry Experience",
+            "desc": ""
+        },
+        {
+            "title": "Organized Design Process",
+            "desc": ""
+        },
+        {
+            "title": "Expert in Key Skills",
+            "desc": ""
+        },
+        {
+            "title": "Always Meet Deadlines",
+            "desc": ""
+        }
+    ],
+    "service": [
+        {
+            "title": "Design and Development",
+            "desc": "Starting from single page landing sites to e-commerce sites, I help you visualize what you think, in the form of full-fledged websites"
+        },
+        {
+            "title": "Responsive Design",
+            "desc": "Fully responsive design compatible to most of the devices"
+        },
+        {
+            "title": "UX Research",
+            "desc": "To give your project a kick-start. I'll provide you detailed UX research as of my 3+ years of UX experience"
+        },
+        {
+            "title": "Scalable Code",
+            "desc": "Provides the extendable, readable code for the future changes"
+        },
+        {
+            "title": "SEO Strategies",
+            "desc": "A good copy does what thousands of better designs can't. That, along with other strategies like SEO, AdSense, Analytics etc. can take you far - in the long run."
+        }
+    ],
+    "skills": {
+        "desc": "Over 6 month's of experience working with these skills and providing a best solution to the client",
+        "images" : ["image/html.svg", "image/css.svg","image/js.svg","image/mongo.svg","image/express.svg","image/react.svg","image/node.svg","image/redux.svg","image/bootstrap.svg","image/tailwind.svg","image/material.png", "image/firebase.png","image/next.svg"],
+    },
+        "projects": [
+            {
+            },
+             {
+                "title": "Teacher and Student Dash Board",
+                "keySkills": ["React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P3 Teacher and Student Dashboard.jpeg",
+                "url": "https://vocal-pithivier-f0c935.netlify.app"
+            },
+            {
+                "title": "URL Shortner",
+                "keySkills": ["React","Nodjs", "Javascript","HTML", "CSS", "Bootstrap",],
+                "image": "P2  URL Shortner.jpeg",
+                "url": " https://chipper-figolla-bdfe4e.netlify.app/"
+            },
+            {
+                "title": "SB2 Admin",
+                "keySkills": ["React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P4 SB2 Admin.jpeg",
+                "url": " https://fastidious-cajeta-282a04.netlify.app"
+            },
+            {
+                "title": "Beweries",
+                "keySkills": ["React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P5 Beweries.jpeg",
+                "url": "https://wondrous-palmier-b3edef.netlify.app"
+            },
+            {
+                "title": "Authentication",
+                "keySkills": ["MongoDB", "Nodejs" ,"React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P6 Authentication.jpeg",
+                "url": " https://gleeful-florentine-a6c02d.netlify.app/login"
+            },
+            {
+                "title": "MarkDown",
+                "keySkills": ["Nodejs" ,"Express", "React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P7 MarkDown.jpeg",
+                "url": " https://super-dolphin-1b0b62.netlify.app/"
+            },
+            {
+                "title": "Online Shop",
+                "keySkills": ["React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P8 Online Shop.jpeg",
+                "url": "https://dancing-pastelito-daf1b0.netlify.app/"
+            },
+            {
+                "title": "Price Cart",
+                "keySkills": ["Nodejs" ,"Express", "React", "Javascript","HTML", "CSS", "Bootstrap"],
+                "image": "P9 Price Cart.jpeg",
+                "url": "https://spectacular-halva-36eca7.netlify.app/"
+            },
+            
+        ]
+}
+
+export default data;
